b5ffc79d9cce3c9abe5c31733c1c7aadb0178541
leninkumar <leninkumarpothabattula@gmail.com>
2020-06-05 10:16:45 +0530

    Complete part 2 of Excercise#1

--- Excercise#1/quizgame.go
+++ Excercise#1/quizgame.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
+	// Read command line flags
+	filePath := flag.String("path", "problems.csv", "a string")
+	duration := flag.Int("wait", 30, "a string")
+	flag.Parse()
 	// Open file for reading
-	file, err := os.Open("./problems.csv")
+	file, err := os.Open(*filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,10 +35,24 @@ func main() {
 		}
 		numQuestions++
 		fmt.Printf("Quiz %v: %v\n", numQuestions, record[0])
-		var answer string
-		fmt.Scanln(&answer)
-		if answer == record[1] {
-			correctAnswers++
+		input := make(chan string, 1)
+		go func() {
+			var msg string
+			fmt.Scan(&msg)
+			input <- msg
+		}()
+		exit := make(chan bool, 1)
+		go func(duration *int) {
+			time.Sleep(time.Second * time.Duration(*duration))
+			exit <- true
+		}(duration)
+		select {
+		case answer := <-input:
+			if answer == record[1] {
+				correctAnswers++
+			}
+		case <-exit:
+			fmt.Println("User didn't give any answer. Proeeding to next question")
 		}
 	}
 	fmt.Printf("You got %v out of %v questions correct\n", correctAnswers, numQuestions)
