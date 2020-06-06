4538dbd715b5807f94fea616386ece0a303da17a
leninkumar <leninkumarpothabattula@gmail.com>
2020-06-06 09:25:10 +0530

    refactored code

--- Excercise#1/quizgame.go
+++ Excercise#1/quizgame.go
@@ -4,56 +4,70 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
-	"log"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
 	// Read command line flags
-	filePath := flag.String("path", "problems.csv", "a string")
-	duration := flag.Int("wait", 30, "a string")
+	filePath := flag.String("filepath", "problems.csv", "a csv file in the format of question,answer")
+	duration := flag.Int("waitTime", 30, "a string")
 	flag.Parse()
 	// Open file for reading
 	file, err := os.Open(*filePath)
 	if err != nil {
-		log.Fatal(err)
+		exit(fmt.Sprintf("Failed to open csv file: %s\n", *filePath))
 	}
 	defer file.Close()
-	// Read the CSV file until end
+	// Read the contents of CSV file
 	reader := csv.NewReader(file)
-	var numQuestions int = 0
-	var correctAnswers int = 0
-	for {
-		record, err := reader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		numQuestions++
-		fmt.Printf("Quiz %v: %v\n", numQuestions, record[0])
-		input := make(chan string, 1)
+	lines, err := reader.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided csv file")
+	}
+	problems := parseLines(lines)
+	// Print the questoin and Take user input
+	correct := 0
+	timer := time.NewTimer(time.Duration(*duration) * time.Second)
+loop:
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s=", i+1, p.question)
+		input := make(chan string)
 		go func() {
 			var msg string
 			fmt.Scan(&msg)
 			input <- msg
 		}()
-		exit := make(chan bool, 1)
-		go func(duration *int) {
-			time.Sleep(time.Second * time.Duration(*duration))
-			exit <- true
-		}(duration)
 		select {
 		case answer := <-input:
-			if answer == record[1] {
-				correctAnswers++
+			if answer == p.answer {
+				correct++
 			}
-		case <-exit:
-			fmt.Println("User didn't give any answer. Proeeding to next question")
+		case <-timer.C:
+			break loop
 		}
 	}
-	fmt.Printf("You got %v out of %v questions correct\n", correctAnswers, numQuestions)
+	fmt.Printf("\n%d out of %d are correct\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			question: line[0],
+			answer:   strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	question string
+	answer   string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
