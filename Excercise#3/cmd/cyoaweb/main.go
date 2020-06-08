10772b97563c60e0fc9580896ec9b9fe50949d48
leninkumar <leninkumarpothabattula@gmail.com>
2020-06-08 14:37:00 +0530

    Wrapping up choose your own adventure code

--- Excercise#3/cmd/cyoaweb/main.go
+++ Excercise#3/cmd/cyoaweb/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
@@ -14,7 +15,7 @@ import (
 func main() {
 	port := flag.Int("port", 8080, "Port on choose your own adventure runs")
 	jsonfile := flag.String("json", "gophers.json", "Json file which has stories")
-	// htmlfile := flag.String("html", "layout.html", "HTML to render stories")
+	htmlfile := flag.String("html", "temp.html", "HTML to render stories")
 	flag.Parse()
 	// open json file which contains stories
 	file := utils.OpenFile(*jsonfile)
@@ -23,9 +24,21 @@ func main() {
 		utils.Exit("Not able to read file")
 	}
 	// create template using htmlfile
-	// t := template.Must(template.ParseFiles(*htmlfile))
+	t := template.Must(template.ParseFiles(*htmlfile))
 	// handler := handler.DefaultHandler(t, story)
-	h := handler.NewHandler(story)
+	mux := http.NewServeMux()
+	h := handler.NewHandler(story, handler.WithTemplate(t), handler.WithPathParseFunc(pathParseFunc()))
+	mux.Handle("/story/", h)
 	fmt.Printf("Server started listening on port :%d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
+}
+
+func pathParseFunc() func(r *http.Request) string {
+	return func(r *http.Request) string {
+		path := r.URL.Path
+		if path == "/story" || path == "/story/" {
+			path = "/story/intro"
+		}
+		return path[len("/story/"):]
+	}
 }
--- Excercise#3/handler/handler.go
+++ Excercise#3/handler/handler.go
@@ -2,30 +2,67 @@ package handler
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 
 	"../cyoa"
 )
 
 var tmp *template.Template
+var defaultPathParseFunc func(r *http.Request) string
 
 func init() {
 	tmp = template.Must(template.ParseFiles("layout.html"))
+	defaultPathParseFunc = DefaultPathParseFunc
 }
 
-func NewHandler(s cyoa.Story) http.Handler {
-	return handler{story: s}
+type HandlerOpts func(h *handler)
+
+func WithTemplate(t *template.Template) HandlerOpts {
+	return func(h *handler) {
+		h.t = t
+	}
+}
+
+func WithPathParseFunc(fn func(r *http.Request) string) HandlerOpts {
+	return func(h *handler) {
+		h.pathFunc = fn
+	}
+}
+
+func NewHandler(s cyoa.Story, opts ...HandlerOpts) http.Handler {
+	h := handler{story: s, t: tmp, pathFunc: defaultPathParseFunc}
+	for _, opt := range opts {
+		opt(&h)
+	}
+	return h
 }
 
 type handler struct {
-	story cyoa.Story
+	story    cyoa.Story
+	t        *template.Template
+	pathFunc func(r *http.Request) string
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := tmp.Execute(w, h.story["intro"])
-	if err != nil {
-		panic(err)
+	path := h.pathFunc(r)
+	if chapter, ok := h.story[path]; ok {
+		err := h.t.Execute(w, chapter)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
 	}
+	http.Error(w, "Chapter not found...", http.StatusNotFound)
+}
+
+func DefaultPathParseFunc(r *http.Request) string {
+	path := r.URL.Path
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+	return path[1:]
 }
 
 func DefaultHandler(t *template.Template, story cyoa.Story) http.Handler {
